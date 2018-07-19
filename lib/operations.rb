cf04d734f27e5086e1d321a56e2979ec89f495a3
Amee Covarrubias <ameecova@yahoo.com>
2018-07-19 12:49:34 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,7 @@
 def unsafe?(speed)
-  speed > 60? true : false end
-  if speed < 40? true : false end
-  if speed.between?(40,60) false : true end
+  speed > 60? true : false 
+  speed < 40? true : false 
+  speed.between?(40,60) false : true 
 end
 
 
