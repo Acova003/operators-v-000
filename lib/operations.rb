6777983e84510eb0ce9101ed13394b9a1bc447d5
Amee Covarrubias <ameecova@yahoo.com>
2018-07-19 12:53:59 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,10 @@
 def unsafe?(speed)
-  speed > 60? true : false 
-  speed < 40? true : false 
-  speed > 40 && speed < 60? false : true 
+  if speed > 60 
+    true
+  elsif speed < 40 
+    true 
+  else speed > 40 && speed < 60 
+    false
 end
 
 
